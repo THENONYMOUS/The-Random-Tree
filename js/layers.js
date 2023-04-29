e3716d49e2ceada17619577c378abb872a4adc72
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-29 17:20:24 +0100

    hopefully fixed a bug

--- js/layers.js
+++ js/layers.js
@@ -73,7 +73,7 @@ addLayer("e", {
     requires: new Decimal(100), // Can be a function that takes requirement increases into account
     resource: "entanglement", // Name of prestige currency
     baseResource: "superpositions", // Name of resource prestige is based on
-    baseAmount() {return player[p.layer].points}, // Get the current amount of baseResource
+    baseAmount() {return player['p'.layer].points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
