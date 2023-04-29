282efe18bd97881b9ffb47d67dbf579bc3a3a3a3
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-29 17:12:42 +0100

    hopefully fixed a thing

--- js/layers.js
+++ js/layers.js
@@ -64,7 +64,7 @@ addLayer("p", {
 addLayer("e", {
     name: "entanglement", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -73,7 +73,7 @@ addLayer("e", {
     requires: new Decimal(100), // Can be a function that takes requirement increases into account
     resource: "entanglement", // Name of prestige currency
     baseResource: "superpositions", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player[p.layer].points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -83,7 +83,7 @@ addLayer("e", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "e", description: "E: Reset for entanglement", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
