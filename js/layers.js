5fa4116513a68a181218572879048fcf1d22acfa
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-29 17:00:26 +0100

    added new layer

--- js/layers.js
+++ js/layers.js
@@ -61,3 +61,42 @@ addLayer("p", {
         },
     },
 })
+addLayer("e", {
+    name: "entanglement", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#561FBD",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "entanglement", // Name of prestige currency
+    baseResource: "superpositions", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.6, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "e", description: "E: Reset for entanglement", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Multiply point gain based on points",
+            description: "huh",
+            cost: new Decimal(1),
+            effect () {
+                return player.points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" }
+        },
+    },
+})
