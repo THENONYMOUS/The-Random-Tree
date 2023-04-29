73d6222d90baf06151752a8625b5c55498d76aa7
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-29 17:06:26 +0100

    fixed a hotkey

--- js/layers.js
+++ js/layers.js
@@ -23,7 +23,7 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "s", description: "P: Reset for superpositions", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for superpositions", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
     upgrades: {
