fd8083ba9e3cb37ff234fbfdc3921413751a6b7c
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-29 14:46:25 +0100

    added upgrade

--- js/layers.js
+++ js/layers.js
@@ -39,6 +39,15 @@ addLayer("p", {
                 return player.points.add(1).pow(0.1)
             },
             effectDisplay() { return format (upgradeEffect(this.layer, this.id)) },
-        }
+        },
+        13: {
+            title: "Increase point gain based on SP",
+            description: "Interesting...",
+            cost: new Decimal(20),
+            effect () {
+                return player[this.layer].points.add(1).pow(0.3)
+            },
+            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" },
+        },
     },
 })
