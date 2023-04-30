19c09856f9c6b3cac5aac39be4082a9b89d64fde
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-30 08:12:33 +0000

    changed challenge reward

--- js/mod.js
+++ js/mod.js
@@ -48,7 +48,7 @@ function getPointGen() {
 	if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
 	if (hasUpgrade('e', 11)) gain = gain.times(upgradeEffect('e', 11))
 	if (inChallenge('e', 11)) gain = gain.pow(0.55)
-	if (hasChallenge('e', 11)) gain = gain.pow(1.1)
+	if (hasChallenge('e', 11)) gain = gain.times(rewardEffect('e', 11))
 	return gain
 }
 
