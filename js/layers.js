7ab5da04670469b205856508af48016a8f66eca8
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-04-30 19:31:30 +0100

    pray

--- js/layers.js
+++ js/layers.js
@@ -2,21 +2,23 @@ addLayer("p", {
     name: "superpositions", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "SP", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
+    startData() {
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "superpositions", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() { return player.points }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade('p', 14)) mult = mult.times(upgradeEffect('p', 14))
-        if (hasUpgrade('p',22)) mult = mult.times(upgradeEffect('p', 22))
+        if (hasUpgrade('p', 22)) mult = mult.times(upgradeEffect('p', 22))
         if (hasMilestone('e', 0)) mult = mult.pow(1.05)
         if (hasUpgrade('p', 21)) mult = mult.pow(1.1)
         return mult
@@ -26,9 +28,9 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for superpositions", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        { key: "p", description: "P: Reset for superpositions", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
     ],
-    layerShown(){return true},
+    layerShown() { return true },
     upgrades: {
         11: {
             title: "Increase point gain by 2",
@@ -42,25 +44,25 @@ addLayer("p", {
             effect() {
                 return player.points.add(1).pow(0.3)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
         },
         13: {
             title: "Increase point gain based on SP",
             description: "Interesting...",
             cost: new Decimal(20),
-            effect () {
+            effect() {
                 return player[this.layer].points.add(1).pow(0.35)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
         },
         14: {
             title: "Increase SP gain based on points",
             description: "",
             cost: new Decimal(50),
-            effect () {
+            effect() {
                 return player.points.add(1).pow(0.1)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
         },
         21: {
             title: "Raise SP mult to the power ^1.1",
@@ -71,10 +73,10 @@ addLayer("p", {
             title: "Multiply SP gain based on SP",
             description: "",
             cost: new Decimal(2500),
-            effect () {
+            effect() {
                 return player.p.points.add(1).pow(0.05)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x"},
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" },
         },
     },
 })
@@ -82,15 +84,17 @@ addLayer("e", {
     name: "entanglement", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
+    startData() {
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
     color: "#561FBD",
     requires: new Decimal(100), // Can be a function that takes requirement increases into account
     resource: "entanglement", // Name of prestige currency
     baseResource: "superpositions", // Name of resource prestige is based on
-    baseAmount() {return player["p"].points}, // Get the current amount of baseResource
+    baseAmount() { return player["p"].points }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -103,27 +107,27 @@ addLayer("e", {
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "e", description: "E: Reset for entanglement", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        { key: "e", description: "E: Reset for entanglement", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
     ],
-    layerShown(){return true},
+    layerShown() { return true },
     upgrades: {
         11: {
             title: "Multiply point gain based on points",
             description: "huh",
             cost: new Decimal(1),
-            effect () {
+            effect() {
                 return player.points.add(1).pow(0.3)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x" }
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" }
         },
         12: {
             title: "Multiply Entanglement gain based on SP",
             description: "^0.1",
             cost: new Decimal(250),
-            effect () {
+            effect() {
                 return player.p.points.add(1).pow(0.07)
             },
-            effectDisplay() { return format (upgradeEffect(this.layer, this.id))+"x"}
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" }
         },
     },
     milestones: {
@@ -140,7 +144,7 @@ addLayer("e", {
         2: {
             requirementDescription: "50 Entanglement",
             effectDescription: "Unlock a second Challenge",
-            done() { return player.e.points.gte(50)},
+            done() { return player.e.points.gte(50) },
         },
     },
     challenges: {
@@ -148,18 +152,18 @@ addLayer("e", {
             name: "slowdown",
             challengeDescription: "Point gain is raised to the power ^0.55",
             goalDescription: "reach 1,000 points",
-            rewardDescription: function() {return ("Point gain is boosted based on entanglements. Currently: x"+(player.e.points.add(1).times(100).pow(0.25)))},
-            unlocked: function() { return hasMilestone('e', 1)},
-            canComplete: function() {return player.points.gte("1e3")},
+            rewardDescription: function () { return ("Point gain is boosted based on entanglements. Currently: x"+format(player.e.points.add(1).times(100).pow(0.25))) },
+            unlocked: function () { return hasMilestone('e', 1) },
+            canComplete: function () { return player.points.gte("1e3") },
         },
         12: {
             name: "softcapped",
             challengeDescription: "Point gain is divided by current points",
-            goalDescription: function() {return (challengeCompletions('e', 12).add(1).times(500))},
-            rewardDescription: function() {return ("Increase Base gain based on Entanglement. Currently: +"+(player.e.points.pow(0.3).times(challengeCompletions('e', 12))))},
+            goalDescription: function () { return format (Decimal.times(challengeCompletions('e', 12).add(1)), 500) },
+            rewardDescription: function () { return "Increase Base gain based on Entanglement. Currently: +"+format(player.e.points.pow(0.3).times(challengeCompletions('e', 12))) },
             completionLimit: 3,
-            unlocked: function() {return hasMilestone('e', 2)},
-            canComplete: function() {return player.points.gte(challengeCompletions('e', 12).add(1).times(500))},
+            unlocked: function () { return hasMilestone('e', 2) },
+            canComplete: function () { return player.points.gte(challengeCompletions('e', 12).add(1).times(500)) },
         },
     },
 })
