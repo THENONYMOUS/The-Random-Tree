b571bfa3d8833a32132764009eacbfeb74677e9e
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-05-02 18:51:06 +0100

    unNaNned c2

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function getPointGen() {
 	let gain = new Decimal(5)
 	gain = gain.add(new Decimal(challengeCompletions('cp', 11)).times(5))
 	gain = gain.times(player.cp.points.times(new Decimal(challengeCompletions('cp', 12))).add(1).pow(0.5))
-	if (inChallenge('cp', 12)) gain = gain.dividedBy(player.points.pow(0.5))
+	if (inChallenge('cp', 12)) gain = gain.dividedBy(player.points.add(1).pow(0.5))
 	return gain
 }
 
