997b9614f40292cb80605135018815e69b3bf445
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-05-07 14:19:01 +0000

    changed exponent

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("s", {
     baseResource: "snow", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.75, // Prestige currency exponent
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if(hasUpgrade('s', 14)) mult=mult.times(upgradeEffect('s', 14))
