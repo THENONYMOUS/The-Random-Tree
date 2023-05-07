56b6072fd3e2a7728e74a8824ab3e1a45d2c05aa
THENONYMOUS <109756501+THENONYMOUS@users.noreply.github.com>
2023-05-07 12:51:05 +0100

    added basic stuff

--- js/mod.js
+++ js/mod.js
@@ -13,13 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Snow",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
+		- Added snow.<br>
 		- Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
